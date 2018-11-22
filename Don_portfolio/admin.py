e65704dd49700703237e52b86d7634126a438bd3
mortada gzar <mortadagzar@gmail.com>
2018-11-21 18:50:00 -0800

    Update admin.py

--- Don_portfolio/admin.py
+++ Don_portfolio/admin.py
@@ -2,16 +2,4 @@ from django.contrib import admin
 from django.db import models
 from .models import Post
 
-admin.site.register(Post)
-huhu
-uhu
-guuhuhuhu\
-vyg
 
-uggugugug
-
-
-ctfftftf
-guhuuu
-guuuu
-vgygygygyg
