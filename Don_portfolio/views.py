d7481f4d7af27d0588914b26b425500816e5e5c8
mortada gzar <mortadagzar@gmail.com>
2018-11-23 21:06:00 -0800

    Update views.py

--- Don_portfolio/views.py
+++ Don_portfolio/views.py
@@ -48,7 +48,7 @@ def contact(request):
             return redirect('successView')
     return render(request, "contact.html", {'form': form})
 
-hhghgh ygfgygyg
+hhghgh ygfgygygyyyy
 
 def successView(request):
     return render(request, "successView.html")
